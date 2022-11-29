1436f1c6916b55651b9c3fd068be68a96baafdf1
Genevieve <lastwitchofthecentury@gmail.com>
2022-11-28 22:52:13 -0500

    Moved non-code files to its own folder, made the Character class with getters and setters, and renamed main to not conflict with class name

---
+++ Character.java
@@ -0,0 +1,29 @@
+public class Character {
+    private int health;
+    private int power;
+    private int defense;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+}
