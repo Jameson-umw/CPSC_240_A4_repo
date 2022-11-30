3e9cd2639d5dbd5bc6b2b7250d295a1d336d6f4b
lpowell2 <103131236+lpowell2@users.noreply.github.com>
2022-11-30 18:36:30 -0500

    Adding frame stuff

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame=new JFrame("Final Project");
+        JPanel panel=(JPanel) frame.getContentPane();
+        panel.setPreferredSize(new Dimension(1300,650));
+        panel.setLayout(null);
+        Canvas canvas=new Canvas();
+        canvas.setBounds(0,0,1300,650);
+        canvas.setIgnoreRepaint(true);
+        panel.add(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
